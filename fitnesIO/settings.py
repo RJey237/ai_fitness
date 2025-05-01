63971941585cf99ba24f1ecb5909d686afe0a21d
Erkaboy0313 <fayyozbekov20@gmail.com>
2025-05-01 20:41:41 +0500

    fixed merge conflict

--- fitnesIO/settings.py
+++ fitnesIO/settings.py
@@ -12,11 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
-<<<<<<< HEAD
 AUTH_USER_MODEL = 'user.CustomUser'
-=======
 from dotenv import load_dotenv
->>>>>>> umar
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
