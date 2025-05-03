b6bae5c4f1b2d8a97611eed07133fd34525692e7
Erkaboy0313 <fayyozbekov20@gmail.com>
2025-05-03 19:45:40 +0500

    merge conflict

--- user/migrations/0001_initial.py
+++ user/migrations/0001_initial.py
@@ -36,13 +36,8 @@ class Migration(migrations.Migration):
                 ('phone', models.CharField(blank=True, max_length=20, null=True, verbose_name='phone')),
                 ('profile_image', models.ImageField(upload_to='profile image')),
                 ('is_verified', models.BooleanField(default=False)),
-<<<<<<< HEAD
                 ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.permission', verbose_name='user permissions')),
-=======
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='customuser_groups', related_query_name='customuser', to='auth.group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='customuser_permissions', related_query_name='customuser', to='auth.permission', verbose_name='user permissions')),
->>>>>>> javlon
             ],
             options={
                 'verbose_name': 'user',
